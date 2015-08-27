906727a13795b874e2d2b922741fa82df125410d
Amin Karaji <amin.karaji@siteminder.com>
2015-08-28 09:32:24 +1000

    fix for feature rspec

--- spec/features/libraries_spec.rb
+++ spec/features/libraries_spec.rb
@@ -3,8 +3,8 @@ require 'rails_helper'
 feature "Libraries" do
   feature "GET /libraries" do
     scenario "works! (now write some real specs)" do
-      get libraries_path
-      expect(response).to have_http_status(200)
+      visit libraries_path
+      expect(page).to have_content("Listing Libraries")
     end
   end
 end
